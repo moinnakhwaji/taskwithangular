c00967cb211d0ac2f3b1fa7057c44f9659446f62
moin <moinnakhwaji@gmail.com>
2025-05-23 23:40:37 +0530

    .

--- backend/firebase/firebaseAdmin.js
+++ backend/firebase/firebaseAdmin.js
@@ -1,11 +1,23 @@
-// backend/firebase/firebaseAdmin.js
+import * as admin from 'firebase-admin';
 
-import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json";
+console.log("GOOGLE_PROJECT_ID:", process.env.GOOGLE_PROJECT_ID);
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      type: 'service_account',
+ 
+
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      auth_uri: process.env.GOOGLE_AUTH_URI,
+      token_uri: process.env.GOOGLE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+      client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+    }),
   });
 }
 
